f9a383edc7d78355947c5202c071598446985388
Chathuranga Wijesinghe <crwijesinghe@gmail.com>
2020-02-04 18:34:57 +0800

    85

---
+++ src/components/info.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Info extends Component {
+  render() {
+    return (
+      <div>
+        <h2>The Info</h2>
+        <p>React is awesome!</p>
+      </div>
+    );
+  }
+}
+
+export default Info;
