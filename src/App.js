a36646c2c0b41a7cf70f285555a41f99d1440a97
Chathuranga Wijesinghe <crwijesinghe@gmail.com>
2020-02-05 11:35:59 +0800

    95

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
-import Info from "./components/info";
+import User from "./components/User";
+import Account from "./components/Account";
+import Blog from "./components/Blog";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <Info />
-      </div>
+      <Router>
+        <Route exact path="/" component={User}></Route>
+        <Route path="/account" component={Account}></Route>
+        <Route path="/blog" component={Blog}></Route>
+      </Router>
     );
   }
 }
